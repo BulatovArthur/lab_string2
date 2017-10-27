6f87ecbe846035051350cd199e2b720d96732f52
BulatovArthur <32384736+BulatovArthur@users.noreply.github.com>
2017-10-27 09:09:19 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main (int argc, char* argv[])
 {
  string str1=argv[1];
  char symbhol = argv[2][0];
- for (int i=0; i< str1.length (); i++)
+ for (int i=0; i < str1.length (); i++)
  {
         if (str1[i]==symbhol)
         cout << i+1 << endl;
